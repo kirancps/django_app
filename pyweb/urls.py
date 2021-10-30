15e504b1ab0c6c9a5d2899ac559e3de6bd3f45c9
Kiran <scikiran@gmail.com>
2021-10-30 20:43:48 +0530

    added import views from users module

--- pyweb/urls.py
+++ pyweb/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include
 from django.urls import path
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
