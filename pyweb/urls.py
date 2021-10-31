af993f830fa6c4aec4675fe2c38992b2031e89f6
Kiran <scikiran@gmail.com>
2021-10-31 11:42:48 +0530

    added url paths

--- pyweb/urls.py
+++ pyweb/urls.py
@@ -21,4 +21,9 @@ from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login-page'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout-page'),
+    path('register/',user_view.register,name='register'),
+    path('profile/', user_view.profile, name='profile'),
+    path('',user_view.home, name='home'),
 ]
